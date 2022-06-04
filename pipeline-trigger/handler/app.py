ce1a5082e53e27c30216cd14f6fe3f3bf65e3469
Lerna Ekmekcioglu <elerna@amazon.com>
2022-06-04 18:23:04 -0400

    updates

--- pipeline-trigger/handler/app.py
+++ pipeline-trigger/handler/app.py
@@ -4,17 +4,22 @@ def lambda_handler(event, context):
     print(event)
     githubEventPayload=json.loads(event['body'])
     modifiedFiles = githubEventPayload["commits"][0]["modified"]
+    print("modified files")
+    print(modifiedFiles)
     #full path
     for filePath in modifiedFiles:
         # Extract folder name
         folderName = (filePath[:filePath.find("/")])
+        print(folderName)
         break
 
     #start the pipeline
     if len(folderName)>0:
+        print("inside if")
         # Codepipeline name is foldername. 
         # We can read the configuration from S3 as well. 
         if not exists_pipeline(folderName):
+            print("pipeline does not exist")
             create_pipeline(folderName)
         returnCode = start_code_pipeline(folderName)
 
@@ -27,12 +32,13 @@ def lambda_handler(event, context):
 def start_code_pipeline(pipelineName):
     client = codepipeline_client()
     print('starting pipeline ',pipelineName)
-    response = client.start_pipeline_execution(name=pipelineName)
+    response = client.start_pipeline_execution(name=pipelineName, parameter_overrides={})
     print('start_pipeline_execution response ',response)
     return True
 
 def create_pipeline(pipelineName):
     client = codebuild_client()
+    print('creating pipeline')
     response = client.start_build(projectName='create_pipeline',
                                   environmentVariablesOverride=[
                                         {
