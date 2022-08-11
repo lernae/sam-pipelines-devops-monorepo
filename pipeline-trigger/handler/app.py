561999d7ce6d606f6febece8ad3fe6ff63f511e0
Lerna Ekmekcioglu <elerna@amazon.com>
2022-08-11 19:52:18 -0400

    input validation

--- pipeline-trigger/handler/app.py
+++ pipeline-trigger/handler/app.py
@@ -4,46 +4,48 @@ import time
 
 def lambda_handler(event, context):
     print(event)
-    githubEventPayload=json.loads(event['body'])
-    modifiedFiles = githubEventPayload["commits"][0]["modified"] + githubEventPayload["commits"][0]["added"] + githubEventPayload["commits"][0]["removed"]
-    print("modified files")
-    print(modifiedFiles)
-    # All folders containing modified files in this commit
-    folders = set(map(lambda s: (s[:s.find("/")]), modifiedFiles))
-    print(folders)
+    if 'body' in event:
+        githubEventPayload=json.loads(event['body'])
+        if 'commits' in event['body']:
+            committedfiles = githubEventPayload["commits"][0]["modified"] + githubEventPayload["commits"][0]["added"] + githubEventPayload["commits"][0]["removed"]
+            print("Files in commit")
+            print(committedfiles)
+            # All folders containing modified files in this commit
+            folders = set(map(lambda s: (s[:s.find("/")]), committedfiles))
+            print(folders)
 
-    if len(folders) > 0:
-        client = codepipeline_client()
-        resp = client.list_pipelines()
-        pipelines = resp['pipelines']
-        print(pipelines)
-        pipelineNames = [p['name'] for p in pipelines]
+            if len(folders) > 0:
+                client = codepipeline_client()
+                resp = client.list_pipelines()
+                pipelines = resp['pipelines']
+                print(pipelines)
+                pipelineNames = [p['name'] for p in pipelines]
 
-        pipelinesToCreate = list(set(folders) - set(pipelineNames))
-        print(pipelinesToCreate)
-        pipelinesToStart = list(set(folders) - set(pipelinesToCreate))
-        print(pipelinesToStart)
-        for folderName in pipelinesToStart:
-            print('starting pipeline for ', folderName)
-            client.start_pipeline_execution(name=folderName)
-            print('started pipeline for ', folderName)
+                pipelinesToCreate = list(set(folders) - set(pipelineNames))
+                print(pipelinesToCreate)
+                pipelinesToStart = list(set(folders) - set(pipelinesToCreate))
+                print(pipelinesToStart)
+                for folderName in pipelinesToStart:
+                    print('starting pipeline for ', folderName)
+                    client.start_pipeline_execution(name=folderName)
+                    print('started pipeline for ', folderName)
 
-        cbclient = codebuild_client()
-        projectName='create_pipeline'
-        for folderName in pipelinesToCreate:
-            print('pipeline ', folderName, ' does not exist')
-            print('creating pipeline')
-            cbclient.start_build(projectName=projectName,
-                                 environmentVariablesOverride=[{
-                                        'name': 'ENV_PIPELINE_NAME',
-                                        'value': folderName,
-                                        'type': 'PLAINTEXT'
-                                }])
+                cbclient = codebuild_client()
+                projectName='create_pipeline'
+                for folderName in pipelinesToCreate:
+                    print('pipeline ', folderName, ' does not exist')
+                    print('creating pipeline')
+                    cbclient.start_build(projectName=projectName,
+                                         environmentVariablesOverride=[{
+                                                'name': 'ENV_PIPELINE_NAME',
+                                                'value': folderName,
+                                                'type': 'PLAINTEXT'
+                                        }])
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Modified project in repo:')
-        }
+                return {
+                    'statusCode': 200,
+                    'body': json.dumps('Modified project in repo:')
+                }
     return {
         'statusCode': 200,
         'body': json.dumps('No modified project in repo')
